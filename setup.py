13c600fc180ce72c8b1ab53b24bb8a02e28b4387
Paul-Tru <99089100+Paul-Tru@users.noreply.github.com>
2024-10-08 11:50:34 -0700

    add max distance stop slider, changing values

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import os
 
-from pygame.transform import threshold
-from sqlalchemy import column
-
 try:
     import customtkinter as ctk
     import configparser
@@ -38,9 +35,6 @@ class Setup:
         self.controller()
         self.app.mainloop()
 
-        if not self.debug:
-            import robot_controll as rc
-
         try:
             self.rob_ip = self.config["IP"]["rob"]
             self.pc_ip = self.config["IP"]["pc"]
@@ -48,17 +42,24 @@ class Setup:
             self.rob_ip = None
             self.pc_ip = None
 
+    def save_config(self):
+        with open("config.ini", "w") as configfile:
+            self.config.write(configfile)
+
     def ip_config(self):
         frame = ctk.CTkFrame(self.app)
-        frame.grid(column=0, row=0, padx=self.frame_pad, pady=self.frame_pad)
+        frame.grid(column=0, row=0, sticky="n", padx=self.frame_pad, pady=self.frame_pad)
 
         label = ctk.CTkLabel(frame, text="Wifi", font=self.frame_font)
         label.pack(padx=self.comp_pad, pady=self.comp_pad)
 
-        self.entry_rob_ip = ctk.CTkEntry(frame, placeholder_text="Robot IP")
+        rob_ip_ini = self.config.get("IP", "rob", fallback="")
+        pc_ip_ini = self.config.get("IP", "pc", fallback="")
+
+        self.entry_rob_ip = ctk.CTkEntry(frame, placeholder_text=rob_ip_ini)
         self.entry_rob_ip.pack(padx=self.comp_pad, pady=self.comp_pad)
 
-        self.entry_pc_ip = ctk.CTkEntry(frame, placeholder_text="Computer IP")
+        self.entry_pc_ip = ctk.CTkEntry(frame, placeholder_text=pc_ip_ini)
         self.entry_pc_ip.pack(padx=self.comp_pad, pady=self.comp_pad)
 
         status = ctk.CTkLabel(frame, text="❌ Not Connected")
@@ -70,15 +71,22 @@ class Setup:
             if rob_ip is not None:
                 self.rob_ip = rob_ip
                 self.pc_ip = pc_ip
-
+            else:
+                self.rob_ip = rob_ip_ini
+                self.pc_ip = pc_ip_ini
+                
             try:
+                import robot_controll as rc
                 check = rc.check_conn(self.rob_ip, self.pc_ip)
             except:
                 check = False
 
-            if check or self.debug:
+            if check:
                 frame.configure(fg_color="#302c2c")
                 status.configure(text="✅ Connected")
+            elif self.debug:
+                frame.configure(fg_color="#302c2c")
+                status.configure(text="DEBUG")
             else:
                 frame.configure(fg_color="darkred")
                 status.configure(text="❌ Not Connected")
@@ -96,6 +104,7 @@ class Setup:
                 self.config["GENERAL"]["debug"] = "True"
             else:
                 self.config["GENERAL"]["debug"] = "False"
+            self.save_config()
             print(value)
 
         switch = ctk.CTkSwitch(frame, text="Debug", command=change)
@@ -114,25 +123,48 @@ class Setup:
         label.pack(padx=self.comp_pad, pady=self.comp_pad)
 
         self.max_speed()
+        self.max_distance()
 
     def max_speed(self):
         frame = ctk.CTkFrame(self.right_frame)
         frame.pack(padx=self.comp_pad, pady=self.comp_pad)
 
-        speed = self.config["ROBOT"]["max_speed"]
+        max_speed = self.config["ROBOT"]["max_speed"]
 
-        label = ctk.CTkLabel(frame, text=f"Max Speed: {speed}")
+        label = ctk.CTkLabel(frame, text=f"Max Speed: {max_speed} rpm")
         label.pack(padx=self.comp_pad, pady=self.comp_pad)
 
+        
         def get(value):
             max_speed = int(value)  # Slider value is passed automatically
-            label.configure(text=f"Max Speed: {max_speed}")  # Update label with formatted value
+            label.configure(text=f"Max Speed: {max_speed} rpm")  # Update label with formatted value
             self.config["ROBOT"]["max_speed"] = str(max_speed)
+            self.save_config()
             print("Max Speed: " + str(max_speed))
 
         slider = ctk.CTkSlider(frame, number_of_steps=10, to=5000, command=get)
         slider.pack(padx=self.comp_pad, pady=self.comp_pad)
-        slider.set(int(speed))
+        slider.set(int(max_speed))
+
+    def max_distance(self):
+        frame = ctk.CTkFrame(self.right_frame)
+        frame.pack(padx=self.comp_pad, pady=self.comp_pad)
+
+        max_distance = self.config["ROBOT"]["max_distance"]
+
+        label = ctk.CTkLabel(frame, text=f"Max Distance: {max_distance} cm")
+        label.pack(padx=self.comp_pad, pady=self.comp_pad)
+
+        def get(value):
+            max_distance = int(value)  # Slider value is passed automatically
+            label.configure(text=f"Max Distance: {max_distance} cm")  # Update label with formatted value
+            self.config["ROBOT"]["max_distance"] = str(max_distance)
+            self.save_config()
+            print("Max Speed: " + str(max_distance))
+
+        slider = ctk.CTkSlider(frame, number_of_steps=6, to=300, command=get)
+        slider.pack(padx=self.comp_pad, pady=self.comp_pad)
+        slider.set(int(max_distance))
 
     def controller(self):
         self.controller_frame = ctk.CTkFrame(self.app)
@@ -148,17 +180,26 @@ class Setup:
         frame.pack(padx=self.frame_pad, pady=self.frame_pad)
 
         threshold = self.config["CONTROLLER"]["threshold"]
+        max_speed = self.config["ROBOT"]["max_speed"]
+        #steps = 
+        to=int(max_speed)/2
+        steps = to/50
 
-        label = ctk.CTkLabel(frame, text=f"Threshold: {threshold}")
+        label = ctk.CTkLabel(frame, text=f"Threshold: {threshold}/{max_speed}\n \n"
+                             f"{str(round(int(threshold)/int(max_speed)*100))}%")
         label.pack(padx=self.comp_pad, pady=self.comp_pad)
 
         def get(value):
-            threshold = int(value)  # Slider value is passed automatically
-            label.configure(text=f"Threshold: {threshold}")  # Update label with formatted value
+            threshold = int(value)
+            max_speed = self.config["ROBOT"]["max_speed"]  # Slider value is passed automatically
+            label.configure(text=f"Threshold: {threshold}/{max_speed}\n \n"
+                            f"{str(round(int(threshold)/int(max_speed)*100))}%")
             self.config["CONTROLLER"]["threshold"] = str(threshold)
+            self.save_config()
+            slider.configure(to=int(max_speed)/2, number_of_steps = to/50)
             print("Threshold: "+ str(threshold))
 
-        slider = ctk.CTkSlider(frame, number_of_steps=4, to=200, command=get)
+        slider = ctk.CTkSlider(frame, number_of_steps=steps, to=to, command=get)
         slider.pack(padx=self.comp_pad, pady=self.comp_pad)
         slider.set(int(threshold))
 
